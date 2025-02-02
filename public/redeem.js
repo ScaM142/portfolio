facfb2f01de330c03e60eae244cd58782c84d2f3
Marek Šutý <mareksuty123@gmail.com>
2025-02-02 11:31:41 +0100

    Code enter

---
+++ public/redeem.js
@@ -0,0 +1,1 @@
+// filepath: /c:/Users/marek/Desktop/react projekty/portfolio/portfolio/public/redeem.js
--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,12 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import RedeemPage from './components/RedeemPage';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-
     <Router>
       <Routes>
-        {/* ...existing routes... */}
         <Route path="/" element={
           <div className="min-h-screen bg-white">
             <Header />
@@ -28,24 +25,9 @@ function App() {
             <Footer />
           </div>
         } />
-
-        <Route path="/redeem" element={<RedeemPage />} />
-        {/* ...existing routes... */}
+        <Route path="/redeem" element={<a href="/redeem.html">Go to Redeem Page</a>} />
       </Routes>
     </Router>
-
-    /*
-        <div className="min-h-screen bg-white">
-          <Header />
-          <main>
-            <Hero />
-            <About />
-            <Skills />
-            <Projects />
-            <Contact />
-          </main>
-          <Footer />
-        </div>*/
   );
 }
 
